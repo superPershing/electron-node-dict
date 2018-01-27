0e8ba1ac7fd3128df2b709081ef5dc341256998c
superPershing <727342027@qq.com>
2018-01-27 23:59:21 +0800

    parse json to html

--- dict-app/public/javascripts/translate.js
+++ dict-app/public/javascripts/translate.js
@@ -31,21 +31,21 @@ $(function () {
     //     $('#translate-result').text(data['trans_result'][0]['dst'])
     //   }
     // })
-    
+
     var $word = $('#word').val()
     var query = $word
     // 有道翻译API
-    var appSecret = ''; //应用密钥 
-    var appID = '';//应用ID 
-    var salt = (new Date).getTime();
+    var appSecret = 'ZkBGtajsNvWat6byR00ptQ9PSKdo2H8N' // 应用密钥
+    var appID = '12b7c2d926c74565'// 应用ID
+    var salt = (new Date()).getTime()
     // var query = 'test';
     // 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
     // var from = 'zh-CHS';
     // var to = 'en';
-    var from = 'en';
-    var to = 'zh-CHS';
-    var str1 = appID + query + salt + appSecret;
-    var sign = MD5(str1);
+    // var from = 'en';
+    // var to = 'zh-CHS';
+    var str1 = appID + query + salt + appSecret
+    var sign = MD5(str1)
     $.ajax({
       url: 'http://openapi.youdao.com/api',
       type: 'post',
@@ -54,14 +54,112 @@ $(function () {
         q: query,
         appKey: appID,
         salt: salt,
-        from: from,
-        to: to,
+        // from: from,
+        // to: to,
         sign: sign
       },
       success: function (data) {
-        console.log(data);
-      }
-    });
+        console.log(data)
 
+        parseJsonCreateDom(data)
+      }
+    })
   })
+
+  function parseJson (data) {
+    // parse json
+    if (data['errorCode'] == 0) {
+      console.log(data['query'] + ' ' + data['translation'][0])
+      // phonetic
+      var u = data['basic']['us-phonetic']
+      var e = data['basic']['uk-phonetic']
+      var c = data['basic']['phonetic']
+
+      if (u != null && e != null) {
+        console.log('(U: ' + u + ')')  // us-phonetic
+        console.log('(E: ' + e + ')')  // uk-phonetic
+      } else if (c != null) {
+        console.log('(PinYin:  ' + c + ')')  // phonetic
+      }
+
+      // explains
+      var explains = data['basic']['explains']
+      if (explains != null) {
+        for (var i in explains) {
+          console.log(explains[i])
+        }
+      }
+
+      // phrase
+      var phrase = data['web']
+      if (phrase != null) {
+        for (var p in phrase) {
+          console.log(phrase[p]['key'] + ':  ' + phrase[p]['value'].toString())
+        }
+      }
+    }
+  }
+
+    function parseJsonCreateDom (data) {
+        // parse json and create dom for translate-result Div
+        if (data['errorCode'] == 0) {
+            var lastDiv = document.getElementById("translate-result")
+            lastDiv.parentNode.removeChild(lastDiv)
+
+            var currentDiv = document.createElement("div")
+            document.body.appendChild(currentDiv)
+            currentDiv.id = "translate-result"
+            currentDiv = document.getElementById("translate-result")
+
+            // console.log(data['query'] + ' ' + data['translation'][0])
+            var translationP = document.createElement("p")
+            var translationContent = document.createTextNode(data['query'] + ' ' + data['translation'][0])
+            translationP.appendChild(translationContent)
+            currentDiv.appendChild(translationP)
+
+            // phonetic
+            var u = data['basic']['us-phonetic']
+            var e = data['basic']['uk-phonetic']
+            var c = data['basic']['phonetic']
+
+            if (u != null && e != null) {
+                // console.log('(U: ' + u + ')')  // us-phonetic
+                // console.log('(E: ' + e + ')')  // uk-phonetic
+                var phoneticP = document.createElement("p")
+                var phoneticContent = document.createTextNode('(U: ' + u + ') (E: ' + e + ')')
+                phoneticP.appendChild(phoneticContent)
+                currentDiv.appendChild(phoneticP)
+            } else if (c != null) {
+                // console.log('(PinYin:  ' + c + ')')  // phonetic
+                var phoneticP = document.createElement("p")
+                var phoneticContent = document.createTextNode('(PinYin:  ' + c + ')')
+                phoneticP.appendChild(phoneticContent)
+                currentDiv.appendChild(phoneticP)
+            }
+
+            // explains
+            var explains = data['basic']['explains']
+            if (explains != null) {
+                for (var i in explains) {
+                    // console.log(explains[i])
+                    var explainP = document.createElement("p")
+                    var explainContent = document.createTextNode(explains[i])
+                    explainP.appendChild(explainContent)
+                    currentDiv.appendChild(explainP)
+                }
+            }
+
+            // phrase
+            var phrase = data['web']
+            if (phrase != null) {
+                for (var p in phrase) {
+                    // console.log(phrase[p]['key'] + ':  ' + phrase[p]['value'].toString())
+                    var phraseP = document.createElement("p")
+                    var phraseContent = document.createTextNode(phrase[p]['key'] + ':  ' + phrase[p]['value'].toString())
+                    phraseP.appendChild(phraseContent)
+                    currentDiv.appendChild(phraseP)
+                }
+            }
+        }
+    }
 })
